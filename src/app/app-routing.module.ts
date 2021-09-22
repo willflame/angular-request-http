499ac388af25296db457b67f4050009088766cea
Willian Anselmo da Silva <willian0506@hotmail.com>
2021-09-22 10:51:36 -0300

    Create initial project and added routes and bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cursos',
+    // loadChildren: './cursos/cursos.module#CursosModule'
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  },
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  { path: '**', redirectTo: 'cursos' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
